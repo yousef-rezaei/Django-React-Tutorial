84a2f28c330a54cbd4d6bd2da2c73a2445f508fe
yousef <yousef.rezaei9@gmail.com>
2025-06-02 00:55:25 +0200

    changed deployment.py

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -1,17 +1,12 @@
 import os
-from .settings import *
-from pathlib import Path
-# from .settings import BASE_DIR
+# from .settings import *
+from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 SECRET_KEY = os.environ['MY_SECRET_KEY']
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
@@ -59,6 +54,5 @@ DATABASES = {
        
     }
 }
-# Static files (CSS, JavaScript, Images)
+# we dont need beacuse we get it from settings.py
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,6 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-3g2@$&g*h*prmggkw9l3+ulp^g$1$aq@kf$feh-v$%6#2zpeux'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -112,8 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+# STATIC_URL = 'static/'
+# using for local development and production
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
