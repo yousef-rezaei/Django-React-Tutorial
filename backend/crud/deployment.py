c1131fe06884f62a7a4bbe7960e0127d1277177a
yousef <yousef.rezaei9@gmail.com>
2025-06-04 01:27:45 +0200

    new changes

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urlparse, parse_qs
 # from .settings import *
 from .settings import BASE_DIR
 
@@ -39,20 +40,38 @@ STORAGES = {
 }
 
 
-CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-CONNECTION_STR = {pair.split('=')[0]: pair.split('=')[1] for pair in CONNECTION.split(' ')}
-# Database
+# CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+# CONNECTION_STR = {pair.split('=')[0]: pair.split('=')[1] for pair in CONNECTION.split(' ')}
+# # Database
 
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": CONNECTION_STR["dbname"],
+#         "HOST": CONNECTION_STR["host"],
+#         "USER": CONNECTION_STR["user"],
+#         "PASSWORD": CONNECTION_STR["password"],
+       
+#     }
+# }
+
+url = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+parsed = urlparse(url)
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": CONNECTION_STR["dbname"],
-        "HOST": CONNECTION_STR["host"],
-        "USER": CONNECTION_STR["user"],
-        "PASSWORD": CONNECTION_STR["password"],
-       
+        "NAME": parsed.path[1:],  # remove leading slash
+        "USER": parsed.username,
+        "PASSWORD": parsed.password,
+        "HOST": parsed.hostname,
+        "PORT": parsed.port,
+        "OPTIONS": {
+            "sslmode": parse_qs(parsed.query).get("sslmode", ["require"])[0]
+        }
     }
 }
+
 # we dont need beacuse we get it from settings.py
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
